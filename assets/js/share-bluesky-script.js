35bfa95ba2fa6a391e2a5afac12c12085e6f94cf
ConcernedCitizen2025 <mr.benjaminparsons@gmail.com>
2025-01-25 08:29:22 -0800

    ghjk

--- assets/js/share-bluesky-script.js
+++ assets/js/share-bluesky-script.js
@@ -5,5 +5,14 @@ function shareToBluesky(title, url) {
     window.open(shareUrl, '_blank');
 }
 
+function copyBlueskyShare(title, url) {
+    const shareText = `${title}: ${url}`;
+    navigator.clipboard.writeText(shareText).then(() => {
+        alert('Page details copied! Open Bluesky and paste to share.');
+    }).catch(err => {
+        console.error('Failed to copy text: ', err);
+        alert('Failed to copy text. Please try again.');
+    });
+}
 
 
