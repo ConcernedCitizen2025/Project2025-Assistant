0b64b984cda46ce0babf8bc20fcc96421147e2c3
ConcernedCitizen2025 <mr.benjaminparsons@gmail.com>
2025-01-25 08:25:22 -0800

    hj

--- assets/js/share-bluesky-script.js
+++ assets/js/share-bluesky-script.js
@@ -1,7 +1,9 @@
-<script>
+
 function shareToBluesky(title, url) {
     const text = `${title}: ${url}`;
     const shareUrl = `https://bsky.app/compose?text=${encodeURIComponent(text)}`;
     window.open(shareUrl, '_blank');
 }
-</script>
+
+
+
