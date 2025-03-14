df8d4d197d810714abe826978f0f5cb9e91b7317
ConcernedCitizen2025 <mr.benjaminparsons@gmail.com>
2025-03-14 09:22:14 -0700

    juik

--- assets/js/california-protest-map.js
+++ assets/js/california-protest-map.js
@@ -812,8 +812,11 @@ document.addEventListener("DOMContentLoaded", function() {
         },
         { city: "Washington, DC", coords: [38.9072, -77.0369], link: "https://nowmarch.org/", date: "2025-03-14" },
         { 
+            event: "Veterans Rally"
+            where: "Bakersfield V.A. Clinic"
+            time: "12:00 PM (Noon) to 2:00 PM"
             city: "Bakersfield, CA", 
-            coords: [35.3733, -119.0187], 
+            coords: [35.375586, -119.043355], 
             link: "https://www.bakersfield.com/news/local-veteran-to-lead-protest-against-va-cuts/article_10c9353e-ff7b-11ef-bef0-bbeb6658fdca.html", 
             date: "2025-03-14", 
             forceInclude: true 
