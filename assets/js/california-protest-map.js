5a8f922f0a8db03ae6dbace9c816823bdc4d70f3
ConcernedCitizen2025 <mr.benjaminparsons@gmail.com>
2025-03-14 00:05:40 -0700

    4256

--- assets/js/california-protest-map.js
+++ assets/js/california-protest-map.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const protestLocations = [
         
         { city: "Washington, DC", coords: [38.9072, -77.0369], link: "https://nowmarch.org/", date: "2025-03-14" },
-        { city: "Bakersfield, CA", coords: [35.3733, -119.0187], link: "https://www.bakersfield.com/news/local-veteran-to-lead-protest-against-va-cuts/article_10c9353e-ff7b-11ef-bef0-bbeb6658fdca.html", date: "2025-03-14" },
+        { city: "Bakersfield, CA", coords: [35.3733, -119.0187], link: "https://events.pol-rev.com/events/bakersfield-va-protest", date: "2025-03-14" }
         { city: "Irvine, CA", coords: [33.6846, -117.8265], link: "https://events.pol-rev.com/events/b9d30c6b-9649-4b92-9a86-d4af6d6c15c3", date: "2025-03-09" },
         { city: "Irvine, CA", coords: [33.6846, -117.8265], link: "https://events.pol-rev.com/events/e96254d3-d538-4b54-b70a-79771517b62b", date: "2025-03-16" },
         { city: "San Luis Obispo, CA", coords: [35.2828, -120.6596], link: "https://events.pol-rev.com/events/9c2d9347-f5fc-4f2e-a1f4-30c1bd48282f", date: "2025-03-08" },
