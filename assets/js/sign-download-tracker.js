bc6c3b1aedda57c5e4a1f862d63aa451c2c54f84
ConcernedCitizen2025 <mr.benjaminparsons@gmail.com>
2025-06-16 16:28:45 -0700

    ghjk

--- assets/js/sign-download-tracker.js
+++ assets/js/sign-download-tracker.js
@@ -1,14 +1,13 @@
-<script>
+// assets/js/sign-download-tracker.js
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll('.sign-download').forEach(link => {
-    link.addEventListener('click', () => {
-      if (window.gtag) {
+  document.querySelectorAll('.sign-download').forEach(function(link) {
+    link.addEventListener('click', function() {
+      if (typeof gtag === 'function') {
         gtag('event', 'sign_download', {
           event_category: 'Protest Sign',
-          event_label: link.dataset.sign,
+          event_label: link.dataset.sign
         });
       }
     });
   });
 });
-</script>
