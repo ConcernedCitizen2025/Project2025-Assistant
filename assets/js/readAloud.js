e33fd53bbbe48fa4161ef3652608e0f8bec766d9
ConcernedCitizen2025 <mr.benjaminparsons@gmail.com>
2025-06-22 01:56:41 -0700

    hjk

--- assets/js/readAloud.js
+++ assets/js/readAloud.js
@@ -17,12 +17,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const picker = document.getElementById("voicePickerContainer");
   if (picker) {
     const wrap = document.createElement("div");
-    /*wrap.innerHTML = `
+    wrap.innerHTML = `
       <label style="font-size:0.9em; margin-left:1em;">
-        <input type="checkbox" id="nativeVoiceToggle"/>
-        Use system voices
       </label>
-    `;*/
+    `;
     picker.appendChild(wrap);
     document.getElementById("nativeVoiceToggle")
       .addEventListener("change", e => {
